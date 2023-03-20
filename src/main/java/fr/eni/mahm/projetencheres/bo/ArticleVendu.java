1a98936bd3cc406c95634b9365706c1c14aad691
Ragnarohf <mathieuthoumire@hotmail.fr>
2023-03-20 16:45:10 +0100

    Pull class articleVendu
    ajout de la classe  articleVendu

---
+++ src/main/java/fr/eni/mahm/projetencheres/bo/ArticleVendu.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package fr.eni.mahm.projetencheres.bo;
+
+/**
+ * @author           Thoumire Mathieu
+ * @description      Classe modélisant la vente d'un article
+ * @date             20-03-2023
+ * @version			 POO - V1.0
+ */
+public class ArticleVendu {
+
+}
