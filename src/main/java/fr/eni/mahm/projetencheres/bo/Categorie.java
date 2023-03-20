8b10380279795bcc7f837ac3f5c6628b7e966c68
mathieu <mathieu.perin.prog@gmail.com>
2023-03-20 18:11:33 +0100

    Create Categorie.java

---
+++ src/main/java/fr/eni/mahm/projetencheres/bo/Categorie.java
@@ -0,0 +1,35 @@
+package fr.eni.mahm.projetencheres.bo;
+
+/**
+ * Classe categorisant une enchere
+ * @author mathi
+ * @version 1.0.0
+ */
+public class Categorie {
+	private int noCategorie ;
+	private String libelle;
+	
+	//-------------------- CONSTRUCTOR ZONE --------------------//
+
+	public Categorie(int noCategorie, String libelle) {
+	this.noCategorie = noCategorie;
+	this.libelle = libelle;
+}
+	//-------------------- GETTER/SETTER ZONE --------------------//
+
+	public int getNoCategorie() {
+		return noCategorie;
+	}
+
+	public void setNoCategorie(int noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+}
