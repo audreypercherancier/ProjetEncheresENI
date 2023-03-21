c3751708a70e4f8cef5276d44abae6438a07faf0
mathieu <mathieu.perin.prog@gmail.com>
2023-03-21 16:20:46 +0100

    push script db user

---
+++ src/main/webapp/WEB-INF/Documents/projetEncheres.org/04-ModelePhysique/MySql/insert utilisateur mathieuP.sql
@@ -0,0 +1,6 @@
+INSERT INTO utilisateurs (
+	pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur
+    )
+	VALUES (
+    "MFail4562", "PERIN", "Mathieu", "mathieu.perin2022@campus-eni.fr", "0707070707", "4 place president carnot", "38300", "bourgoin-jallieu",1234, 0, 1
+    );
