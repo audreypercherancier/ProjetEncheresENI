1e132cd45dc7bcd95eb8c81a56633ae3a4d7e201
Ragnarohf <mathieuthoumire@hotmail.fr>
2023-03-21 16:41:23 +0100

    push requete sql

---
+++ src/main/webapp/WEB-INF/Documents/projetEncheres.org/04-ModelePhysique/MySql/insert utilisateur admin .sql
@@ -0,0 +1,8 @@
+INSERT INTO utilisateurs (
+	pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur
+    )
+	VALUES (
+    "MFail4562", "PERIN", "Mathieu", "mathieu.perin2022@campus-eni.fr", "0707070707", "4 place president carnot", "38300", "bourgoin-jallieu",1234, 0, 1),
+    ("Chou","THOUMIRE","Mathieu","mathieu.thoumire2022@campus-eni.fr", "0203010508", "2 place des accacias", "76200", "Dieppe",1234,0,1),
+    ("AppleGirl","PERCHERANCIER","Audrey","audrey.percherancier2022@campus-eni.fr","0102030405", "18 place de chateau-fort","45258","New-York",1234,0,1),
+    ("DoubleGirl","EL_MOUTAWAKIL","Hind","hind.elmoutawakil2022@campus-eni.fr","0905040203", "7 place des haricotes volants","85456","Tahiti",1234,0,1)
--- src/main/webapp/WEB-INF/Documents/projetEncheres.org/04-ModelePhysique/MySql/insert utilisateur mathieuP.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO utilisateurs (
-	pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur
-    )
-	VALUES (
-    "MFail4562", "PERIN", "Mathieu", "mathieu.perin2022@campus-eni.fr", "0707070707", "4 place president carnot", "38300", "bourgoin-jallieu",1234, 0, 1
-    ;"Chou","THOUMIRE","Mathieu","mathieu.thoumire2022@campus-eni.fr", "0203010508", "2 place des accacias", "76200", "Dieppe",1234,0,1);
-    
