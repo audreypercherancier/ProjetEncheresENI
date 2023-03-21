9e45240ae549f92734c91130c4981e904304b343
Ragnarohf <mathieuthoumire@hotmail.fr>
2023-03-21 16:25:12 +0100

    update insert user

--- src/main/webapp/WEB-INF/Documents/projetEncheres.org/04-ModelePhysique/MySql/insert utilisateur mathieuP.sql
+++ src/main/webapp/WEB-INF/Documents/projetEncheres.org/04-ModelePhysique/MySql/insert utilisateur mathieuP.sql
@@ -3,4 +3,5 @@ INSERT INTO utilisateurs (
     )
 	VALUES (
     "MFail4562", "PERIN", "Mathieu", "mathieu.perin2022@campus-eni.fr", "0707070707", "4 place president carnot", "38300", "bourgoin-jallieu",1234, 0, 1
-    );
+    ;"Chou","THOUMIRE","Mathieu","mathieu.thoumire2022@campus-eni.fr", "0203010508", "2 place des accacias", "76200", "Dieppe",1234,0,1);
+    
