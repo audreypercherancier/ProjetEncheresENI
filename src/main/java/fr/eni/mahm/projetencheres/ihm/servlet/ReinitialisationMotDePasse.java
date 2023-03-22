76f6e2ae132cd983357daff92b5e713481575981
audreypercherancier <audrey03percherancier@gmail.com>
2023-03-22 16:53:26 +0100

    push servlets et jsp mot de passe oublié non fini

---
+++ src/main/java/fr/eni/mahm/projetencheres/ihm/servlet/ReinitialisationMotDePasse.java
@@ -0,0 +1,41 @@
+package fr.eni.mahm.projetencheres.ihm.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ReinitialisationMotDePasse
+ */
+@WebServlet("/reinitialisermdp")
+public class ReinitialisationMotDePasse extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReinitialisationMotDePasse() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		getServletContext().getRequestDispatcher("/WEB-INF/jsp/reinitialisermdp.jsp").forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/reinitialisermdp.jsp
@@ -0,0 +1,41 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+<title>Réinitialisez votre mot de passe</title>
+</head>
+<%@ include file="/WEB-INF/html/entete.html" %>
+
+
+
+<body>
+ <section class="vh-100">
+      <div class="container-fluid h-custom">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-md-9 col-lg-6 col-xl-5">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              class="img-fluid"
+              alt="Sample image"
+            />
+          </div>
+          <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+          </div>
+			<form method="post"  action="reinitialisermdp"  >
+				<label> Veuillez saisir votre adresse mail </label>
+
+						<input type="email" name="email" required>
+
+						<button type="submit" class="btn btn-primary btn-block mb-4"> Valider </button>
+ 		</form>
+ 		</div>
+        </div>
+    </section>
+</body>
+
+<%@ include file="/WEB-INF/html/piedDePage.html"%>
+
+</html>
