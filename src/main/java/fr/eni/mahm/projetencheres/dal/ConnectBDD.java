e88ada7aa46b30f6f275fc11b6506c7664a57c42
HindElMouta <hind.elmoutawakil@gmail.com>
2023-03-27 10:31:16 +0200

    je push les dernier modif pour l'affichage des article
    petit modification dans l'affichage des produit

--- src/main/java/fr/eni/mahm/projetencheres/dal/ConnectBDD.java
+++ src/main/java/fr/eni/mahm/projetencheres/dal/ConnectBDD.java
@@ -1,7 +1,8 @@
 package fr.eni.mahm.projetencheres.dal;
 
-import java.sql.Connection; 
+import java.sql.Connection;
 import java.sql.SQLException;
+
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
